d907e1df6d7a9c96c7278c5aa4736ee4ec24c150
VoidBehemoth <VoidBehemoth@gmail.com>
2025-02-01 16:39:02 -0700

    Basic functionality.

---
+++ src/API/NativeAPI.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace NativeLib.API;
+
+public class NativeLoader {
+
+    // Loads a native (.dll, .so, .dylib) file from the Embedded resources of a mod
+    public static void LoadNativeLibrary(string assemblyName, string libName) {
+        string tempPath = Path.Combine(Path.GetTempPath(), libName);
+
+        // Creates temporary file to load
+        using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(assemblyName + ".resources." + libName))
+        using (FileStream fileStream = new FileStream(tempPath, FileMode.Create, FileAccess.Write))
+        {
+            stream.CopyTo(fileStream);
+        }
+
+        IntPtr handle = IntPtr.Zero;
+
+        Console.WriteLine("[NativeLib] Loading: " + libName);
+
+        if (!NativeLib.loadedMods.Contains(assemblyName)) {
+            NativeLib.loadedMods.Add(assemblyName);
+        }
+
+        // Loads the temporary file
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)){
+            handle = LoadLibrary(tempPath);
+        } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+            handle = LinuxDlopen(tempPath, RTLD_NOW);
+        } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+            handle = MacOSDlopen(tempPath, RTLD_NOW);
+        }
+
+        if (handle == IntPtr.Zero)
+        {
+            throw new Exception("Failed to load native library.");
+        }
+    }
+
+    public Boolean hadLoaded(string modNamespace) {
+        return NativeLib.loadedMods.Contains(modNamespace);
+    }
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr LoadLibrary(string lpFileName);
+
+    [DllImport("libdl.so.2", EntryPoint = "dlopen", SetLastError = true)]
+    private static extern IntPtr LinuxDlopen(string filename, int flags);
+
+    [DllImport("libSystem.dylib", EntryPoint = "dlopen", SetLastError = true)]
+    private static extern IntPtr MacOSDlopen(string filename, int flags);
+
+    private const int RTLD_NOW = 2;
+}
--- src/NativeLib.cs
+++ src/NativeLib.cs
@@ -1,11 +1,14 @@
 ﻿using SML;
 using System;
+using System.Collections.Generic;
 
 namespace NativeLib;
 
 [Mod.SalemMod]
-public class ExampleMod
+public class NativeLib
 {
+    public static List<string> loadedMods { get; protected set;} = new List<string>();
+
     public static void Start()
     {
         Console.WriteLine("NativeLib works!");
