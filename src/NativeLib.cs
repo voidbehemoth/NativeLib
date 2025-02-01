469c691cb333a2872a534e0f2e020b2f3c138a09
VoidBehemoth <VoidBehemoth@gmail.com>
2025-02-01 16:21:49 -0700

    Initial commit.

---
+++ src/NativeLib.cs
@@ -0,0 +1,13 @@
+﻿using SML;
+using System;
+
+namespace NativeLib;
+
+[Mod.SalemMod]
+public class ExampleMod
+{
+    public static void Start()
+    {
+        Console.WriteLine("NativeLib works!");
+    }
+}
